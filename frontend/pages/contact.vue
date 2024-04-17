495be4d684909633e7a381823b7826daef70bf46
Debbie Dann <29425781+hellodeborahuk@users.noreply.github.com>
2024-04-17 14:08:06 +0100

    wip

--- frontend/pages/contact.vue
+++ frontend/pages/contact.vue
@@ -1,27 +1,40 @@
+<script lang="ts" setup>
+  import {contactPageQuery} from '../graphql/queries';
+  //import { StrapiBlocks, type BlocksContent } from 'vue-strapi-blocks-renderer';
+
+  const {data} = await useAsyncQuery(contactPageQuery);
+  const page = data.value?.page?.data ?? [];
+  console.log(page)
+  //const VNode = StrapiBlocks({ content: page ?? [] });
+</script>
+
 <template>
-    <div class="container">
-        <p>copy</p>
-        <form>
-            <div class="sm:col-span-3">
-          <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Name</label>
-          <div class="mt-2">
-            <input type="text" name="name" id="name" autocomplete="name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-          </div>
-        </div>
 
-        <div class="sm:col-span-3">
-          <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
-          <div class="mt-2">
-            <input id="email" name="email" type="email" autocomplete="email" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-          </div>
-        </div>
+    <div class="container mb-10 lg:mb-20 lg:w-1/2">
+      <h2>{{page.attributes.segments[0].heading}}</h2>
+      <!-- <p class="mb-6"><VNode /></p> -->
+        <form class="space-y-6">
+            <div>
+              <label for="name" class="block text-sm font-medium leading-6 text-gray-900">{{page.attributes.segments[0].labelValue1}}</label>
+              <div class="mt-2">
+                <input type="text" name="name" id="name" autocomplete="name" class="appearance-none block w-full bg-white rounded-md py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-core-brand-alpha">
+              </div>
+            </div>
 
-        <div class="sm:col-span-4">
-          <label for="message" class="block text-sm font-medium leading-6 text-gray-900">Message</label>
-          <div class="mt-2">
-            <input type="text" name="message" id="message" autocomplete="message" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-          </div>
-        </div>
+            <div>
+              <label for="email" class="block text-sm font-medium leading-6 text-gray-900">{{page.attributes.segments[0].labelValue2}}</label>
+              <div class="mt-2">
+                <input id="email" name="email" type="email" autocomplete="email" class="appearance-none block w-full bg-white rounded-md py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-core-brand-alpha">
+              </div>
+            </div>
+
+            <div>
+              <label for="message" class="block text-sm font-medium leading-6 text-gray-900">{{page.attributes.segments[0].labelValue3}}</label>
+              <div class="mt-2">
+                <textarea name="message" id="message" autocomplete="message" class="appearance-none block w-full bg-white rounded-md py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-core-brand-alpha"></textarea>
+              </div>
+            </div>
+            <button type="submit" class="button-primary">{{page.attributes.segments[0].buttonText}}</button>
         </form>
     </div>
 </template>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,17 +1,17 @@
 <script lang="ts" setup>
 import {singlePageQuery} from '../graphql/queries';
 
-const {data} = await useAsyncQuery(singlePageQuery);
-const page = data.value?.page?.data ?? [];
+const {data, error} = await useAsyncQuery(singlePageQuery, {slug: '/'});
+const page = data.value?.pages.data[0] ?? {};
 </script>
 
 <template>
-  <div v-for="segment in page.attributes.segments">
+  <div v-if="page.attributes" v-for="segment in page.attributes.segments">
     <Hero v-if="segment.__typename === 'ComponentHeroHero'" :description="segment.description" :image="segment.image" :price="segment.price" />
     <Classes v-if="segment.__typename === 'ComponentClassesClasses'" :classes="segment" />
     <TextImageBlock v-if="segment.__typename === 'ComponentContentTextAndImageBlock'" :copy="segment.copy" :image="segment.image" />
     <Newsletter v-if="segment.__typename === 'ComponentContentNewsletter'" :heading="segment.heading" :copy="segment.copy" :placeholderInput1="segment.placeholderInput1" :placeholderInput2="segment.placeholderInput2" :buttonText="segment.buttonText" />
     <ImageGrid v-if="segment.__typename === 'ComponentContentImageGrid'" :images="segment.images" />
+    <ContactForm  v-if="segment.__typename === 'ComponentContactContactForm'" />
   </div>
-
 </template>
