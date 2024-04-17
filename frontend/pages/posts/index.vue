a729cfa13000fa22fe487df0de6d431757e7e46b
Debbie Dann <29425781+hellodeborahuk@users.noreply.github.com>
2024-04-17 15:00:40 +0100

    fixed height on blog listing cards

--- frontend/pages/posts/index.vue
+++ frontend/pages/posts/index.vue
@@ -9,9 +9,9 @@ const articles = data.value?.articles?.data ?? [];
 <template>
     <div class="container my-16">
         <h2 class="mb-12 text-center">Recent blog posts</h2>
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 ">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
             <div v-for="article in articles" :key="article.id">
-                <div class="rounded-md border border-red-200">
+                <div class="rounded-md border border-red-200 h-full">
                     <NuxtLink :to="{ path: `/posts/${article.attributes.slug}`, query: {id: article.id }}">
                         <div class="overflow-hidden block rounded-t-md">
                             <img :src="`http://localhost:1337${article.attributes.featuredImage.data.attributes.url}`" alt="{{ article.attributes.title }}" class="image-hover aspect-video object-center" />
