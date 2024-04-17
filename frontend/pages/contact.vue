a831b0cea97588fa4576c6a3998013d7ad546d10
Debbie Dann <29425781+hellodeborahuk@users.noreply.github.com>
2024-04-17 12:17:25 +0100

    Started contact page

---
+++ frontend/pages/contact.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <p>copy</p>
+        <form>
+            <div class="sm:col-span-3">
+          <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Name</label>
+          <div class="mt-2">
+            <input type="text" name="name" id="name" autocomplete="name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+          </div>
+        </div>
+
+        <div class="sm:col-span-3">
+          <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+          <div class="mt-2">
+            <input id="email" name="email" type="email" autocomplete="email" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+          </div>
+        </div>
+
+        <div class="sm:col-span-4">
+          <label for="message" class="block text-sm font-medium leading-6 text-gray-900">Message</label>
+          <div class="mt-2">
+            <input type="text" name="message" id="message" autocomplete="message" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+          </div>
+        </div>
+        </form>
+    </div>
+</template>
