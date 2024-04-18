42e6d9758176b677bcef3588b60448c864560ce0
Debbie Dann <29425781+hellodeborahuk@users.noreply.github.com>
2024-04-18 09:14:54 +0100

    form created

--- frontend/pages/posts/[slug].vue
+++ frontend/pages/posts/[slug].vue
@@ -1,12 +1,23 @@
 <script lang="ts" setup>
 import {singleArticleQuery} from '../../graphql/queries';
 import { StrapiBlocks, type BlocksContent } from 'vue-strapi-blocks-renderer';
+import {ref} from 'vue'
 
 const route = useRoute();
 const {data} = await useAsyncQuery(singleArticleQuery, {id: route.query.id});
 const post = data.value.article.data.attributes
 
 const VNode = StrapiBlocks({ content: data.value?.article?.data?.attributes?.body ?? {} });
+
+const formData = ref({
+    name: '',
+    message: '',
+})
+
+const submitForm = () => {
+    console.log('hello world')
+    // reset name and message fields
+}
 </script>
 
 <template>
@@ -14,5 +25,29 @@ const VNode = StrapiBlocks({ content: data.value?.article?.data?.attributes?.bod
         <h2>{{post.title}}</h2>
         <img :src="`http://localhost:1337${post.featuredImage.data.attributes.url}`" alt="{{ post.featuredImage.data.attributes.alternativeText }}" />
         <VNode />
+        <div>
+            <h4>Leave a comment</h4>
+            <form @submit.prevent="submitForm" class="space-y-6 lg:w-2/3">
+                <div>
+                    <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Name</label>
+                <div class="mt-2">
+                    <input type="text" name="name" id="name" autocomplete="name" class="appearance-none block w-full bg-white rounded-md py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-core-brand-alpha">
+                </div>
+                </div>
+                <div>
+                <label for="message" class="block text-sm font-medium leading-6 text-gray-900">Comment</label>
+                <div class="mt-2">
+                    <textarea name="message" id="message" autocomplete="message" class="appearance-none block w-full bg-white rounded-md py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-core-brand-alpha"></textarea>
+                </div>
+                </div>
+                <button type="submit" class="button-primary">submit</button>
+            </form>
+        </div>
+        <div>
+            <h4>Comments - if comments then display</h4>
+            <div v-for="comment in comments"></div>
+        </div>
     </div>
+
+   
 </template>
