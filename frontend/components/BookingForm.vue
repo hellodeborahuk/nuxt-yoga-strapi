68674d17d5b694bceb36e4bb52abcc68867ed627
Debbie Dann <29425781+hellodeborahuk@users.noreply.github.com>
2024-04-19 10:53:02 +0100

    styled and checks for availability

--- frontend/components/BookingForm.vue
+++ frontend/components/BookingForm.vue
@@ -1,22 +1,20 @@
 <template>
     <div class="container mb-10 lg:mb-20">
-        <h2>{{ heading }}</h2>
-        <pre>{{ booking }}</pre>
+        <h2 class="m-10 text-center">{{ heading }}</h2>
         <div>
-            <a href="/" >
-                <div class="w-full md:w-2/3 lg:w-1/2 flex justify-between items-center bg-core-brand-beta hover:bg-core-brand-charlie ease-in duration-150 p-6 rounded-md">   
-                    <div>
-                        <h3>{{ booking.class.data.attributes.title }}</h3>
-                        <p class="mb-2">{{ booking.class.data.attributes.description }}</p>
-                        <p>Date: {{ fullDate }} </p>
-                        <p>Time: {{ shortTime}}</p> 
-                    </div>    
-                    <div>
-                        <p>{{ booking.price }}</p>
-                    </div>
-                    
+            <div class="w-full md:w-2/3 lg:w-1/2 flex justify-between items-center bg-core-brand-beta hover:bg-core-brand-charlie ease-in duration-150 p-6 rounded-md">   
+                <div>
+                    <h3>{{ booking.class.data.attributes.title }} <span v-if="!available">- fully booked</span></h3>
+                    <p class="mb-2 italic">{{ booking.class.data.attributes.description }}</p>
+                    <p>{{ booking.location }}</p>
+                </div>    
+                <div>
+                    <p class="whitespace-nowrap">{{ fullDate }} </p>
+                    <p class="mb-2">{{ shortTime}}</p> 
+                    <p>{{ booking.price }}</p>
                 </div>
-            </a> 
+                
+            </div>
         </div>
     </div>
 </template>
@@ -31,4 +29,8 @@
     const event = new Date(booking.timeDate)
     const fullDate = new Intl.DateTimeFormat('en', {dateStyle: 'full'}).format(event)
     const shortTime = new Intl.DateTimeFormat('en', {timeStyle: 'short'}).format(event)
+
+    const available = computed(() => {
+        return booking.availability > 0
+    })
 </script>
